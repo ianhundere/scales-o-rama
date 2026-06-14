8735fb43c7c548693ac832852f87a0681f454721
Ian Hundere <138915+ianhundere@users.noreply.github.com>
2026-06-14 15:25:11 -0700

    feat: add song mode and harden lookup

--- scale-o-rama.py
+++ scale-o-rama.py
@@ -1,7 +1,6 @@
 import random
 import itertools
 import re
-from pprint import pprint
 
 scales = {
     'major': ['CDEFGAB', 'GABCDEF#', 'DEF#GABC#', 'ABC#DEF#G#', 'EF#G#ABC#D#',
@@ -16,7 +15,7 @@ scales = {
     'phrygian': ['CDbEbFGAbBb', 'C#DEF#G#AB', 'DEbFGABbC', 'D#EF#G#A#BC#',
                  'EFGABCD', 'FF#G#A#CC#D#', 'F#GABC#DE', 'GG#A#CDD#F',
                  'G#ABC#D#EF#', 'ABbCDEFG', 'A#BC#D#FF#G#', 'BCDEF#GA'],
-    'lydian': ['CDEF#GAB', 'C#D#FGG#A#C', 'DEF#G#ABC#', 'D#FGAA#CD', 'EF#G#A#BC#D#', 'FGABCDE', 'F#G#A#CC#D#F', 'GABC#DEF#,', 'G#A#CDD#FG',
+    'lydian': ['CDEF#GAB', 'C#D#FGG#A#C', 'DEF#G#ABC#', 'D#FGAA#CD', 'EF#G#A#BC#D#', 'FGABCDE', 'F#G#A#CC#D#F', 'GABC#DEF#', 'G#A#CDD#FG',
                'ABC#D#EF#G#', 'BbCDEFGA', 'BC#D#FF#G#A#'],
     'mixolydian': ['CDEFGABb', 'C#D#E#F#G#A#B', 'DEF#GABC', 'D#E#F##G#A#B#C#',
                    'EF#G#ABC#D', 'FGABbCDEb', 'F#G#A#BC#D#E', 'GABCDEF',
@@ -30,41 +29,92 @@ scales = {
                 'A#BC#D#EF#G#', 'BCDEFGA'],
 }
 
-whatFunc = input('Choose random or lookup? ').lower()
+# one note: A-G + optional accidental (#/b/##/bb); current data only reaches ##, bb kept for input symmetry
+NOTE_RE = r'[A-G](?:##|#|bb|b)?'
 
-while whatFunc not in ('random', 'r', 'lookup', 'l'):
-    whatFunc = input('Please choose only random or lookup. ').lower()
+# data tables for the song mode songwriting challenge
+TIME_SIGNATURES = ['4/4', '3/4', '6/8', '5/4', '7/8', '12/8']
 
-if whatFunc == 'lookup':
-    notes = input('Input notes you\'d like to lookup: ')
+STRUCTURES = ['AABA', 'ABAB', 'verse-chorus-bridge', 'through-composed', 'AABABCB']
 
-    notes = re.findall(r'[A-G][#b]?', notes)
-    matches = []
-    for key in scales.keys():
-        for scale in scales[key]:
-            for note in notes:
-                if note not in scale:
-                    break
+MOODS = ['wistful', 'triumphant', 'melancholy', 'frantic', 'dreamy', 'menacing',
+         'playful', 'nostalgic', 'serene', 'restless', 'euphoric', 'somber']
+
+CONSTRAINTS = ['limit yourself to 3 instruments', 'no cymbals',
+               'loop one 2-bar idea', 'write it in one take',
+               'use no more than 4 chords', 'leave a 4-bar silence somewhere',
+               'every section must change dynamics', 'no lyrics allowed',
+               'build the whole thing from one motif', 'end on an unresolved chord']
+
+
+def generate_challenge():
+    # pick a mode key first so we keep it, then a scale within that mode
+    mode = random.choice(list(scales.keys()))
+    scale = random.choice(scales[mode])
+    match = re.match(NOTE_RE, scale)
+    root = match.group() if match else scale[:1]
+    return {
+        'scale': scale,
+        'root': root,
+        'mode': mode,
+        'tempo': random.randint(60, 180),
+        'time_sig': random.choice(TIME_SIGNATURES),
+        'structure': random.choice(STRUCTURES),
+        'mood': random.choice(MOODS),
+        'constraint': random.choice(CONSTRAINTS),
+    }
+
+
+if __name__ == '__main__':
+    whatFunc = input('Choose random, lookup, or song? ').lower()
+
+    while whatFunc not in ('random', 'r', 'lookup', 'l', 'song', 's'):
+        whatFunc = input('Please choose only random, lookup, or song. ').lower()
+
+    if whatFunc in ('lookup', 'l'):
+        query = re.findall(NOTE_RE, input('Input notes you\'d like to lookup: '))
+        if not query:
+            print('No valid notes entered.')
+        else:
+            # match on parsed note tokens so 'C' doesn't accidentally match 'C#'
+            matches = []
+            for key, group in scales.items():
+                for scale in group:
+                    if all(note in re.findall(NOTE_RE, scale) for note in query):
+                        matches.append((key, scale))
+            if matches:
+                for key, scale in matches:
+                    print('{}: {}'.format(key, scale))
             else:
-                matches.append([key, scale])
-    pprint(matches)
+                print('No scales contain those notes.')
 
-elif whatFunc == 'random':
-    whatScales = input(
-        'Choose Major, Minor, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian, or All: ').lower()
-    # ^asks user to choose from keys in dict
-
-    while whatScales.lower() not in ('minor', 'major', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'all'):
+    elif whatFunc in ('random', 'r'):
         whatScales = input(
-            'Sorry, invalid choice. Choose only from Major, Minor, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian, or All: ').lower()
-    # if the user chooses an incorrect choice, it reiterates the question/choices
+            'Choose Major, Minor, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian, or All: ').lower()
+        # ^asks user to choose from keys in dict
 
-    # a for loop that looks for the key variable in the scales dictionary to match with the correct key of the scales dictionary.
-    for key in scales.keys():
-        if key == whatScales:
-            print(random.choice(scales[key]))
-            print('That\'s a nice sounding {} scale!'.format(key))
-    if whatScales in ['all']:
-        print(random.choice(list(itertools.chain.from_iterable(scales.values()))))
-        print('That\'s a nice sounding random scale!')
-    # ^ an if statement that points the user's answer to the correct key. the final elif statement uses itertools to flatten the dictionary into values to then randomize
+        while whatScales not in ('minor', 'major', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'all'):
+            whatScales = input(
+                'Sorry, invalid choice. Choose only from Major, Minor, Dorian, Phrygian, Lydian, Mixolydian, Aeolian, Locrian, or All: ').lower()
+        # if the user chooses an incorrect choice, it reiterates the question/choices
+
+        if whatScales in scales:
+            print(random.choice(scales[whatScales]))
+            print('That\'s a nice sounding {} scale!'.format(whatScales))
+        elif whatScales == 'all':
+            # dedupe by note content so enharmonic duplicates (e.g. minor == aeolian) aren't double-weighted
+            print(random.choice(list(set(itertools.chain.from_iterable(scales.values())))))
+            print('That\'s a nice sounding random scale!')
+
+    elif whatFunc in ('song', 's'):
+        # song mode hands back a full songwriting brief, not just a scale
+        challenge = generate_challenge()
+        print('--- Your Songwriting Challenge ---')
+        print('Scale:      {} {} ({})'.format(
+            challenge['root'], challenge['mode'].capitalize(), challenge['scale']))
+        print('Tempo:      {} BPM'.format(challenge['tempo']))
+        print('Time sig:   {}'.format(challenge['time_sig']))
+        print('Structure:  {}'.format(challenge['structure']))
+        print('Mood:       {}'.format(challenge['mood']))
+        print('Constraint: {}'.format(challenge['constraint']))
+        print('Now go write something!')
